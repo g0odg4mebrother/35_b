48e5a741ae640a639060c77fe3e5c4a327bdd32f
Djulse <kirilcukvlad30@gmail.com>
2024-09-30 14:10:29 +0900

    Добавьте файлы проекта.

---
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    setlocale(LC_ALL, "rus");
+
+    int x;
+    int z;
+    int y;
+
+    cout << "введите число x\n";
+    cin >> x;
+    cout << "введите число z\n"; 
+    cin >> z;
+    cout << "введите число y\n";
+    cin >> y;
+
+    cout << "otvet" << max (x + y + z/2, x * y * z)+1;
+
+
+
+
+
+
+
+}
